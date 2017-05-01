a5a94800934dfae0a12f2cc2fc074b13f592689c
Jon Wong <jhannwong@gmail.com>
2017-05-01 20:28:42 +0800

    Resume submitted for Udacity resume review
    Jon targeted:
    https://jobs.lever.co/udacity/b812e5f3-381b-4c87-99c5-6a473efc1942

--- js/resumes.js
+++ js/resumes.js
@@ -4,7 +4,7 @@ resumes['gars4!caving'] = {
   bio: {
     name: 'Jon Wong',
     contacts: {
-      mysite: 'udacity-FEND-online-resume?resume=gars4!caving',
+      mysite: 'udacity-FEND-online-resume/?resume=gars4!caving',
     },
 
     welcomeMessage: 'Looking to competently apply my skills learned at Udacity FEND, and to become a prime demonstration of the efficacy of Udacity education. My human language skills will allow me to work with Udacity Blitz teams that are geographically, linguistically and culturally distributed.',
@@ -14,6 +14,6 @@ resumes['gars4!caving'] = {
       ['Frameworks/Libraries', ['jQuery', 'AngularJS', 'Knockout.js', 'Bootstrap/Foundation', 'Grunt/Gulp', 'Jasmine']],
       ['Other', ['Git/GitHub', 'GIMP', 'Jekyll', 'Phonology', 'Cultural mimicry']]],
 
-    biopic: 'images/fixed/fry.jpg'
+    biopic: 'images/fixed/selfie.jpg'
   }
 };
