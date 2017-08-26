aed2d67e05bd34f6d96755f4ac542aa9c8faeaf9
Jon Wong <jhannwong@gmail.com>
2017-08-26 08:56:56 +0800

    Initial attempts to align with JD
    Job description at Mozilla.
    https://careers.mozilla.org/position/gh/610836

--- js/resumes.js
+++ js/resumes.js
@@ -63,3 +63,101 @@ resumes['gars4!caving'] = {
     ]
   }
 };
+
+resumes['brae40!rally'] = {
+  bio: {
+    name: 'Jon Wong',
+    contacts: {
+      mysite: 'udacity-FEND-online-resume/?resume=gars4!caving',
+    },
+
+    welcomeMessage: 'Looking to use my open-source hacking ability to drive innovation at Mozilla. My human language skills will allow me to build teams that are geographically, linguistically and culturally distributed. Have extreme Phonological Awareness (PA).',
+
+    skills: [
+      ['Languages', ['HTML/CSS/JS', 'Python', 'C/C++', 'PHP', 'Less/Sass']],
+      ['Frameworks/Libraries', ['Docker', 'jQuery', 'AngularJS', 'Knockout.js', 'Bootstrap/Foundation', 'Grunt/Gulp', 'Jasmine']],
+      ['Other', ['Git/GitHub', 'GIMP', 'Jekyll', 'Phonology', 'Cultural mimicry']]],
+
+    biopic: 'images/fixed/selfie.jpg'
+  },
+
+  projects: {
+    op: 'prepend',
+    projects: [
+      {
+        title: "Participate in the open source development process",
+        dates: "TBD",
+        description: [
+          "Code reader. Open-source is open book to me.",
+          "Eg: Hacked open Sigma.js in a short time to extend it (technical test for reverse-engineering and open-source ability"
+        ],
+        images: [],
+        url: ''
+      },
+      {
+        title: "Build and containerize web applications",
+        dates: "TBD",
+        description: [
+          "Experience with Docker containers and Docker API.",
+          "Reproducible containers, version-controlled development of containers",
+          "Experience in developing repeatable environments, even for large-scale production ones."
+        ],
+        images: [],
+        url: ''
+      },
+      {
+        title: "Develop and integrate tools to collect, analyze and share data",
+        dates: "TBD",
+        description: [
+          "Hacked open grunt-responsive-images, corrected bug.",
+          "See conversion from: https://github.com/andismith/grunt-responsive-images/issues/115#issuecomment-312274644",
+          "Assessed that grunt-responsive-images's API cannot allow for full utility of GraphicsMagick (or even ImageMagick).",
+          "Integrated a stop-gap measure with grunt-exec.",
+          "Able to revamp grunt-responsive-images to expose full syntax available in GraphicsMagick or ImageMagick, if time and money permits."
+        ],
+        images: [],
+        url: ''
+      },
+      {
+        title: "Agile workflow and culture",
+        dates: "2017",
+        description: [
+          "Migrated from underused SVN to BitBucket",
+          "Installed fluid, agile, emergent leadership; leadership arises from competence",
+          "Installed optimized version of textbook Agile/Scrum",
+          "Built visibility, accountability of all skills within team",
+          "Technical guidance on coding standards, code organization, architecture",
+          "Freed company from developer-lock, initiated rapid code ownership",
+          "Code quality protection via unit tests, rapid emergency code coverage by targetting mid-level functions",
+          "Rapid stop-gap fix for missing documentation: functional specs implicitly documented in unit tests."
+        ],
+        images: ["images/fixed/sigmajs.png"],
+        url: 'github.com/hannwong/jobapp-latize-techtest'
+      },
+      {
+        title: "Sigma.js Hacking (Reverse-engineering)",
+        dates: "2017",
+        description: [
+          "Hacked Sigma.js within hours to extend functionality",
+          "General demonstration of reverse-engineering ability",
+          "Pixel-accurate CSS styling for HTML lists, complex aesthetics"
+        ],
+        images: ["images/fixed/sigmajs.png"],
+        url: 'github.com/hannwong/jobapp-latize-techtest'
+      },
+      {
+        title: "Git Lesson",
+        dates: "2017",
+        description: [
+          "Trim online Git lesson that covers concepts ('what') and usage ('why'); the 'how' can be Googled.",
+          "Built on Jekyll and HTML/CSS/Javascript, and uses Grunt for art direction.",
+          "Reader-friendly features include editable fields (commit IDs) and floating TOC.",
+          "Full yet trim run-through of necessary Git concepts from need-to-know internals to best-convention collaboration techniques.",
+          "Build script allows deployment on private BitBucket repos and publication on GitHub Pages."
+        ],
+        images: ["images/fixed/git.png"],
+        url: 'jon-wong-sutd.github.io/git-lessons/git-lesson/'
+      }
+    ]
+  }
+};
